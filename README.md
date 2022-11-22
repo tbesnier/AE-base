07383df5c5c0b6ab6f4c3a8634577a03eca97206
tbesnier <mr.thomasbesnier@gmail.com>
2022-11-22 16:19:53 +0100

    set up

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # AE-base
 
 This package provides a Python code for 3D generative models.
+
+python COMA_idsplit_pointnet_original.py --test_label=0 --target=1
+
+python data_generation.py --root_dir=/path/to/data_root_dir --dataset=DFAUST --num_valid=100 
---
+++ visualize_predi.py
@@ -0,0 +1,39 @@
+import os
+
+import numpy as np
+import trimesh
+from tqdm import tqdm
+
+def save_viz(dest, path):
+    if os.path.exists(path):
+        ours = np.load(path)
+        for i in tqdm(range(len(ours)), "saving vizualizations"):
+            tri_mesh = trimesh.Trimesh(np.asarray(np.squeeze(ours[i, :, :3])), np.asarray(triangles), process=False)
+            tri_mesh.export(os.path.join(dest, "tst{0:03}.ply".format(i)))
+    else:
+        print("This predictions does not exist")
+
+
+def save_viz_bad(dest, path):
+    if os.path.exists(path):
+        ours = np.load(path)
+        for i in tqdm(range(len(ours)), "saving vizualizations"):
+            tri_mesh = trimesh.convex.convex_hull(np.asarray(np.squeeze(ours[i, :, :3])))
+            tri_mesh.export(os.path.join(dest, "tst{0:03}.ply".format(i)))
+    else:
+        print("This predictions does not exist")
+
+tri = trimesh.load('../Data/COMA/template/template.obj', process=False)
+triangles=tri.faces
+
+model = 'pointnet_original'
+latent = 128
+folder = '../Data/COMA/results_identity/{0}_autoencoder/latent_{1}'.format(model, latent)
+
+dest = os.path.join(folder, 'viz_predictions')
+os.makedirs(dest, exist_ok=True)
+save_viz(dest, os.path.join(folder, 'predictions/predictions.npy'))
+
+dest = os.path.join(folder, 'viz_targets')
+os.makedirs(dest, exist_ok=True)
+save_viz(dest, os.path.join(folder, 'predictions/targets.npy'))
